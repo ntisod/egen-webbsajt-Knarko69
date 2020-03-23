739530bfe6847079057ffff69b4e305c3180dd27
Knarko69 <58597387+Knarko69@users.noreply.github.com>
2020-03-23 10:00:11 +0100

    Update javascript-exempel.js

--- javascript-exempel.js
+++ javascript-exempel.js
@@ -1,3 +1,7 @@
 function authorDialog() {
     alert("Knarko har gjort sidan år 2020");
 }
+function edit() {
+    document.getElementById("rubrik").innerHTML = "Zarko";
+    document.getElementById("stycketext").style.backgroundColor = "#aaf";
+}
