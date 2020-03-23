dfb216a3f35befcf69cf15cf8643233b8f5e63d0
Knarko69 <58597387+Knarko69@users.noreply.github.com>
2020-03-23 12:56:03 +0100

    Update javascript-exempel.js

--- javascript-exempel.js
+++ javascript-exempel.js
@@ -5,3 +5,8 @@ function edit() {
     document.getElementById("rubrik").innerHTML = "Zarko";
     document.getElementById("stycketext").style.backgroundColor = "#aaf";
 }
+function change() {
+pic = "genomskinlig_logga.png"
+//vet inte hur jag ska lägga till en till bild så jag kan slutföra uppgift 4//
+    document.getElementById("myimage").src= pic;
+}
